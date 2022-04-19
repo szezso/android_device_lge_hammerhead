6fa4955d6fcbad67ec15d29e663d443b6f973fb9
z3DD3r <z3dd3r@gmail.com>
2022-04-19 17:09:17 +0300

    hammerhead: hal: Uprev keymaster HIDL HAL to 4.1

--- device.mk
+++ device.mk
@@ -204,8 +204,7 @@ PRODUCT_PACKAGES += \
     Snap
 
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@3.0-impl \
-    android.hardware.keymaster@3.0-service
+    android.hardware.keymaster@4.1-service
 
 # GPS configuration
 PRODUCT_COPY_FILES += \
