ce8f244331bd5c3747ad9465471fd391c4d40fe5
z3DD3r <z3dd3r@gmail.com>
2022-12-19 21:50:50 +0300

    hammerhead: hal: Build drm clearkey plugin AIDL HAL

--- device.mk
+++ device.mk
@@ -303,7 +303,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service \
-    android.hardware.drm@1.4-service.clearkey
+    android.hardware.drm-service.clearkey
 
 PRODUCT_PROPERTY_OVERRIDES += \
     drm.service.enabled=true
