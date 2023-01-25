ab9d57ff64f2a16cdc1c59ca08515cfdefc1a92a
z3DD3r <z3dd3r@gmail.com>
2023-01-25 14:26:50 +0300

    hammerhead: graphics: Enable tripple buffering

--- device.mk
+++ device.mk
@@ -455,7 +455,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # SurfaceFlinger
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.supports_background_blur=0 \
-    ro.surface_flinger.max_frame_buffer_acquired_buffers=2 \
+    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
     ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.vsync_event_phase_offset_ns=7500000 \
     ro.surface_flinger.vsync_sf_event_phase_offset_ns=5000000
